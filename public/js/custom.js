98934a51e8a8f2cffd5811b168cf08fc79178b40
Priyam Brahmbhatt <pbrahmbhatt3@myseneca.ca>
2019-04-12 18:30:53 -0400

    removed carousel script from custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -71,29 +71,6 @@ $(document).ready(function(){
   });
 
   /*====================================================*/
-  /* team SLIDER                                 */
-  /*====================================================*/
-  $("#carouselExample").on("slide.bs.carousel", function (e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $(".carousel-item").length;
-
-      if (idx >= totalItems-(itemsPerSlide-1)) {
-          var it = itemsPerSlide - (totalItems - idx);
-          for (var i=0; i<it; i++) {
-              // append slides to end
-              if (e.direction==="left") {
-                  $(".carousel-item").eq(i).appendTo(".carousel-inner");
-              }
-              else {
-                  $(".carousel-item").eq(0).appendTo(".carousel-inner");
-              }
-          }
-      }
-  });
-
-  /*====================================================*/
   /* TABS INIT                                   */
   /*====================================================*/
   $('.js-tabs a').on('click', function(e) {
